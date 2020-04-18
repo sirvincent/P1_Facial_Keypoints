b304ddef391936f3c3548485798cd86966901c2f
Vincent Post <v.post@condifood.com>
2020-04-18 22:11:47 +0200

    cleared TODOs in the second ipynb, also attached a stored model to this commit

--- models.py
+++ models.py
@@ -13,7 +13,7 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         
-        ## TODO: Define all the layers of this CNN, the only requirements are:
+        ## DONE: Define all the layers of this CNN, the only requirements are:
         ## 1. This network takes in a square (same width and height), grayscale image as input
         ## 2. It ends with a linear layer that represents the keypoints
         ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
@@ -24,14 +24,33 @@ class Net(nn.Module):
         
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
-        
+        self.pool1    = nn.MaxPool2d(2, 2)
+        self.dropout1 = nn.Dropout(0.2)
+
+        self.conv2    = nn.Conv2d(32, 64, 5)
+        self.pool2    = nn.MaxPool2d(2, 2)
+        self.dropout2 = nn.Dropout(0.2)
+
+        self.conv3    = nn.Conv2d(64, 8, 3)
+        self.pool3    = nn.MaxPool2d(2, 2)
+        self.dropout3 = nn.Dropout(0.2)
+
+        # TODO: if we parametrize the above layers we can calculate the size of the Linear layer in place of having magic numbers
+        self.linear = nn.Linear(8 * 25 * 25, 2 * 68)
 
         
-    def forward(self, x):
-        ## TODO: Define the feedforward behavior of this model
-        ## x is the input image and, as an example, here you may choose to include a pool/conv step:
-        ## x = self.pool(F.relu(self.conv1(x)))
-        
-        
-        # a modified x, having gone through all the layers of your model, should be returned
-        return x
+    def forward(self, input_image):
+        ## DONE: Define the feedforward behavior of this model
+        input_image = self.pool1(F.relu(self.conv1(input_image)))
+        input_image = self.dropout1(input_image)
+
+        input_image = self.pool2(F.relu(self.conv2(input_image)))
+        input_image = self.dropout2(input_image)
+
+        input_image = self.pool3(F.relu(self.conv3(input_image)))
+        input_image = self.dropout3(input_image)
+
+        input_image = input_image.view(input_image.size(0), -1)
+        input_image = self.linear(input_image)
+
+        return input_image
